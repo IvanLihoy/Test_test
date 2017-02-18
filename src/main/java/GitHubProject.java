afa720f1037942e7f2a16a99335ceee9fd661fdd
Ivan <lihoy92ivan@gmail.com>
2017-02-18 21:40:22 +0200

    commit

--- src/main/java/GitHubProject.java
+++ src/main/java/GitHubProject.java
@@ -3,6 +3,6 @@
  */
 public class GitHubProject {
     public static void main(String[] args){
-        System.out.print("Hello world!!!");
+        System.out.print("Hello new world!!!");
     }
 }
